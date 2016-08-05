dff293bfb0cf9cff020301d62722f5e58d25de2f
Santiago Cerro López <sacerro@gmail.com>
2016-08-05 10:51:26 +0200

    Major changes
    All theme data is readed from package.json -> config.

--- index.js
+++ index.js
@@ -1,85 +1,100 @@
 var fs = require('fs');
 var Readable = require('stream').Readable;
 var gutil = require('gulp-util');
-var _ = require('lodash');
-var _s = require('underscore.string');
+var _ = require('lodash'); 
 
 var pkg = JSON.parse(fs.readFileSync('package.json', {encoding: 'utf8'}));
 
 module.exports = function (options) {
-  'use strict';
+	'use strict';
 
-  options = _.defaults(options || {}, {
-    path: 'style.css',
-    name: _s.titleize(_s.humanize(pkg.name)),
-    description: pkg.description,
-    version: pkg.version,
-    uri: pkg.homepage,
-    tags: pkg.keywords,
-    author: pkg.author.name,
-    authorUri: pkg.author.url,
-    license: pkg.license,
-    licenseUri: null
-  });
+	options = _.defaults(options || {}, {
+	path: 'style.css',
+	themename: pkg.config.themename,
+	themeURI: pkg.config.themeURI,
+	description: pkg.config.description,
+	author: pkg.config.author,
+	authorURI: pkg.config.authorURI,
+	version: pkg.config.version,
+	license: pkg.config.license,
+	licenseURI: pkg.config.licenseURI,
+	tags: pkg.config.tags,
+	textdomain: pkg.config.textdomain
+	});
 
-  if (!options.path) {
-    throw new gutil.PluginError('gulp-wpstylecss', '`path` is required!');
-  }
+	if (!options.path) {
+		throw new gutil.PluginError('gulp-wpstylecss', '`path` is required!');
+	}
 
-  if (!options.name) {
-    throw new gutil.PluginError('gulp-wpstylecss', '`name` is required!');
-  }
+	if (!options.themename) {
+		throw new gutil.PluginError('gulp-wpstylecss', '`themename` is required!');
+	}
+	if (!options.author) {
+		throw new gutil.PluginError('gulp-wpstylecss', '`author` is required!');
+	}
 
-  /*
-  Theme Name:     <%= options.name %>
-  Description:    <%= options.description %>
-  Version:        <%= options.version %>
-  Theme URI:      <%= options.homepage %>
-  Tags:           <%= options.keywords %>
-  Author:         <%= options.author.name %>
-  Author URI:     <%= options.author.url %>
-  License:        <%= options.license %>
-  License URI:    <%= options.licenseUri %>
-  */
-  var contents = '/*\n';
 
-  contents += 'Theme Name:     ' + options.name + '\n';
+	/*
+	Theme Name:     <%= options.name %>
+	Theme URI:      <%= options.themeURI %>
+	Author:         <%= options.author %>
+	Author URI:     <%= options.authorURI %>
+	Description:    <%= options.description %>
+	Version:        <%= options.version %>
+	License:        <%= options.license %>
+	License URI:    <%= options.licenseURI %>
+	Tags:           <%= options.tags %>
+	Text Domain:    <%= options.textdomain %>
+	*/
+	
+	var contents = '/*\n';
 
-  if (options.description) {
-    contents += 'Description:    ' + options.description + '\n';
-  }
-  if (options.version) {
-    contents += 'Version:        ' + options.version + '\n';
-  }
-  if (options.uri) {
-    contents += 'Theme URI:      ' + options.uri + '\n';
-  }
-  if (options.tags) {
-    contents += 'Tags:           ' + options.tags + '\n';
-  }
-  if (options.author.name) {
-    contents += 'Author:         ' + options.author.name + '\n';
-  }
-  if (options.author.uri) {
-    contents += 'Author URI:     ' + options.author.uri + '\n';
-  }
-  if (options.license) {
-    contents += 'License:        ' + options.license + '\n';
-  }
-  if (options.licenseUri) {
-    contents += 'License URI:    ' + options.licenseUri + '\n';
-  }
+	contents += 'Theme Name:     ' + options.themename + '\n';
+	
+	if (options.themeURI) {
+		contents += 'Theme URI:      ' + options.themeURI + '\n';
+	}
+	
+	contents += 'Author:         ' + options.author + '\n';
+	
+	if (options.authorURI) {
+		contents += 'Author URI:     ' + options.authorURI + '\n';
+	}
+	
+	if (options.description) {
+		contents += 'Description:    ' + options.description + '\n';
+	}
+	
+	if (options.version) {
+		contents += 'Version:        ' + options.version + '\n';
+	} else {
+		contents += 'Version:        ' + '1.0' + '\n';
+	}
+	
+	if (options.license) {
+		contents += 'License:        ' + options.license + '\n';
+	}
+	
+	if (options.licenseURI) {
+		contents += 'License URI:    ' + options.licenseURI + '\n';
+	}
+	
+	if (options.tags) {
+		contents += 'Tags:           ' + options.tags.join(", ") + '\n';
+	}
+	
+	if (options.textdomain) {
+		contents += 'Text Domain:    ' + options.textdomain + '\n';
+	}
+	
+	contents += '*/';
 
-  contents += '*/\n';
-
-  var stream = new Readable({objectMode: true});
-
-  stream.push(new gutil.File({
-    path: options.path,
-    contents: new Buffer(contents)
-  }));
-
-  stream.push(null);
-
-  return stream;
+	var stream = new Readable({objectMode: true});
+	stream._read = function noop() {}; 
+	stream.push(new gutil.File({
+		path: options.path,
+		contents: new Buffer(contents)
+	}));
+	stream.push(null);
+	return stream;
 };
